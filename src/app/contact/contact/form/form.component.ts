fdaaf4e2744bf6cd536a4ee3b913b4a250eab52d
Saban Uenlue [netTrek] <us@netTrek.de>
2018-11-15 16:08:56 +0100

    02_04_Wertaenderung

--- src/app/contact/contact/form/form.component.ts
+++ src/app/contact/contact/form/form.component.ts
@@ -1,18 +1,21 @@
-import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {NgForm, NgModel} from '@angular/forms';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'in-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit, AfterViewInit {
+export class FormComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild ( NgForm )
   ngForm: NgForm;
 
   @ViewChild ( 'username' )
   username: NgModel;
+  private formSub: Subscription;
+  private userSub: Subscription;
 
   constructor() { }
 
@@ -22,6 +25,24 @@ export class FormComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     console.log ( this.ngForm.value );
     console.log ( this.username.value );
+    this.userSub = this.username.valueChanges.subscribe(
+      next => {
+        console.log( 'new username', next );
+      }
+    );
+    this.formSub = this.ngForm.valueChanges.subscribe(
+      next => {
+        console.log( 'form updated', next );
+      }
+    );
   }
 
+  usernameChanged( currentname: string ) {
+    console.log ( 'currentname', currentname  );
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
+    this.formSub.unsubscribe();
+  }
 }

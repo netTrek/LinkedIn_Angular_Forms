daf91f378ac97e1304f6ff9f24df43c442d54d47
Saban Uenlue [netTrek] <us@netTrek.de>
2018-11-15 15:49:32 +0100

    02_03_Datenmodel

--- src/app/contact/contact/form/form.component.ts
+++ src/app/contact/contact/form/form.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm, NgModel} from '@angular/forms';
 
 @Component({
   selector: 'in-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, AfterViewInit {
+
+  @ViewChild ( NgForm )
+  ngForm: NgForm;
+
+  @ViewChild ( 'username' )
+  username: NgModel;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+    console.log ( this.ngForm.value );
+    console.log ( this.username.value );
   }
 
 }

c309f629333d6ddbbea91cc2e08d9708ffece9c3
Saban Uenlue [netTrek] <us@netTrek.de>
2018-11-17 14:31:09 +0100

    07_08_Validaor_werte

--- src/app/my-validators/future.directive.ts
+++ src/app/my-validators/future.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, forwardRef} from '@angular/core';
+import {Directive, forwardRef, Input} from '@angular/core';
 import {AbstractControl, NG_VALIDATORS, ValidationErrors, Validator} from '@angular/forms';
 import {MyValidators} from './my-validators';
 
@@ -14,10 +14,32 @@ import {MyValidators} from './my-validators';
 })
 export class FutureDirective implements Validator {
 
+  private _inFuture: string;
+  private _fn: () => void;
+
   constructor() { }
 
+  get inFuture(): string {
+    return this._inFuture;
+  }
+  @Input()
+  set inFuture(value: string) {
+    this._inFuture = value;
+    if ( value !== '' && !! this._fn ) {
+      this._fn ();
+    }
+  }
+
   validate(control: AbstractControl): ValidationErrors | null {
-    return MyValidators.isFuture()( control );
+    let date: Date;
+    if ( this._inFuture !== '' ) {
+      date = new Date( this._inFuture );
+    }
+    return MyValidators.isFuture( date )( control );
+  }
+
+  registerOnValidatorChange (fn: () => void): void {
+    this._fn = fn;
   }
 
 }

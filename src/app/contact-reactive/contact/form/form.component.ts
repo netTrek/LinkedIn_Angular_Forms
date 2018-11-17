f9d20be6450e84c34d4905f927bbc350cb391821
Saban Uenlue [netTrek] <us@netTrek.de>
2018-11-17 09:13:06 +0100

    06_04_FormBuilder

--- src/app/contact-reactive/contact/form/form.component.ts
+++ src/app/contact-reactive/contact/form/form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'in-form',
@@ -8,29 +8,43 @@ import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/form
 })
 export class FormComponent implements OnInit {
 
-  myForm: FormGroup;
-  username: FormControl;
-  email: FormControl;
-  msg: FormControl;
-  personalInfo: FormGroup;
+  myForm: FormGroup | FormArray;
+  username: FormControl | AbstractControl;
+  email: FormControl | AbstractControl;
+  msg: FormControl | AbstractControl;
+  personalInfo: FormGroup | AbstractControl;
 
-  constructor() {
+  constructor( private fb: FormBuilder ) {
   }
 
   ngOnInit() {
 
-    this.username = new FormControl( '', [
-      Validators.required, Validators.minLength ( 3 )
-    ] );
-    this.email = new FormControl( '', [
-      Validators.required, Validators.email
-    ] );
-    this.msg = new FormControl( '' );
 
-    this.personalInfo = new FormGroup( { name: this.username, email: this.email } );
+    // this.username = new FormControl( '', [
+    //   Validators.required, Validators.minLength ( 3 )
+    // ] );
+    // this.email = new FormControl( '', [
+    //   Validators.required, Validators.email
+    // ] );
+    // this.msg = new FormControl( '' );
+    //
+    // this.personalInfo = new FormGroup( { name: this.username, email: this.email } );
+    //
+    // this.myForm = new FormGroup( {
+    //   personalInfo: this.personalInfo, msg: this.msg } );
 
-    this.myForm = new FormGroup( {
-      personalInfo: this.personalInfo, msg: this.msg } );
+    this.myForm = this.fb.group( {
+      personalInfo: this.fb.group( {
+        name: ['', [Validators.required, Validators.minLength ( 3 )] ],
+        email: ['', [Validators.required, Validators.email ] ],
+      }),
+      msg: [ '' ]
+    });
+
+    this.username = this.myForm.get ( ['personalInfo', 'name'] ) ;
+    this.email = this.myForm.get ( ['personalInfo', 'email'] ) ;
+    this.msg = this.myForm.get ( ['msg'] ) ;
+    this.personalInfo = this.myForm.get ( ['personalInfo'] ) ;
 
   }
 
